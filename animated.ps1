e9de383a8fbd4fae4bd5c5a1da7c7d880ef020e5
Mitchell Shone <32406025+mitchvaux@users.noreply.github.com>
2023-08-30 00:30:03 +0100

    Add files via upload

---
+++ animated.ps1
@@ -0,0 +1,75 @@
+﻿function four
+{
+    clear
+    write-output " ANALYST "
+    write-output "	      "
+    write-output " ///////////"
+    write-output "|          |"
+    write-output "|####  ####|"
+    write-output "| (o )( o) |"  
+    write-output "(   (       )"
+    write-output "|  ######  |"
+    write-output "\   ___    /"
+    write-output " \________/" 
+    sleep -Seconds 1
+}
+
+
+function three
+{
+    clear
+    write-output " DIGITAL FORENSICS "
+    write-output "            "
+    write-output " ///////////"
+    write-output "|####  ####|"
+    write-output "|          |"
+    write-output "| (o )( o) |"  
+    write-output "(   (       )"
+    write-output "|  ######  |"
+    write-output "\  o       /"
+    write-output " \________/" 
+    sleep -Seconds 1
+}
+
+function two
+{
+    clear
+    write-output " CYBER SECURITY "
+    write-output "            "
+    write-output " ///////////"
+    write-output "|####  ####|"
+    write-output "|          |"
+    write-output "| (o )( o) |"  
+    write-output "(   (       )"
+    write-output "|  ######  |"
+    write-output "\    O     /"
+    write-output " \________/" 
+    sleep -Seconds 1
+}
+
+
+function one
+{
+    clear
+    write-output " MITCH SHONE "
+    write-output "            "
+    write-output " ///////////"
+    write-output "|####  ####|"
+    write-output "|          |"
+    write-output "| (o )( o) |"  
+    write-output "(   (       )"
+    write-output "|  ######  |"
+    write-output "\      0   /"
+    write-output " \________/" 
+    sleep -Seconds 1
+}
+
+
+
+# Run animation 100 times
+1..100|%{
+one 
+two 
+three
+four
+}
